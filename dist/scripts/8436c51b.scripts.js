7d42629cac3441e958cc374bebe534ee1f453c80
Coleman Foley <colemanfoley@gmail.com>
2014-04-29 19:20:55 -0700

    Added dist and .gitignore to repo.

---
+++ dist/scripts/8436c51b.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("yeomanTestApp",["ngCookies","ngSanitize"]),angular.module("yeomanTestApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
